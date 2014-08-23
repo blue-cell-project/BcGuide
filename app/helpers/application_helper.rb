6faad962b992beb708e16672b2f9b749bceb6cf8
Antony Falegk <falegk1@gmail.com>
2014-08-23 20:27:50 +0300

    Page Title System

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+#Title Method. Call <%= provide(:title, "Page Title") %> at the beginning of each page to give the title
+  def title(page_title)
+    base_title = "BcGuide Application"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+
 end
